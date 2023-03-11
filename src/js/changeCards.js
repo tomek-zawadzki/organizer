cf2cb3896ce1d8888191636f1e7d5186d7b352c1
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-03-11 20:29:19 +0100

    refactor changeCards function

--- src/js/changeCards.js
+++ src/js/changeCards.js
@@ -6,27 +6,22 @@ const cards = document.querySelectorAll(".card");
 
 export const changeCards = (e) => {
   if (e.target.closest("li").classList.contains("item-tasks")) {
-    cards.forEach((card) => card.classList.remove("active-card"));
-    cards.forEach((card) => card.classList.add("hidden"));
-    cardTasks.classList.remove("hidden");
-    cardTasks.classList.add("active-card");
+    setActiveCard(cardTasks);
   }
   if (e.target.closest("li").classList.contains("item-notes")) {
-    cards.forEach((card) => card.classList.remove("active-card"));
-    cards.forEach((card) => card.classList.add("hidden"));
-    cardNotes.classList.remove("hidden");
-    cardNotes.classList.add("active-card");
+    setActiveCard(cardNotes);
   }
   if (e.target.closest("li").classList.contains("item-wallet")) {
-    cards.forEach((card) => card.classList.remove("active-card"));
-    cards.forEach((card) => card.classList.add("hidden"));
-    cardWallet.classList.remove("hidden");
-    cardWallet.classList.add("active-card");
+    setActiveCard(cardWallet);
   }
   if (e.target.closest("li").classList.contains("item-calendar")) {
-    cards.forEach((card) => card.classList.remove("active-card"));
-    cards.forEach((card) => card.classList.add("hidden"));
-    cardCalendar.classList.remove("hidden");
-    cardCalendar.classList.add("active-card");
+    setActiveCard(cardCalendar);
   }
+};
+
+const setActiveCard = (cardToSet) => {
+  cards.forEach((card) => card.classList.remove("active-card"));
+  cards.forEach((card) => card.classList.add("hidden"));
+  cardToSet.classList.remove("hidden");
+  cardToSet.classList.add("active-card");
 };
